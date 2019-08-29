3345c7ea47da7a2b85b11b302bddd9ad50a7e5c1
pablofermarroquin <pablofermarroquin@gmail.com>
2019-08-29 11:51:38 -0600

    Modificacion carpetas SQLite

---
+++ app/src/main/java/com/example/contactos/ConexionSQLiteHelper.java
@@ -0,0 +1,26 @@
+package com.example.contactos;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ConexionSQLiteHelper extends SQLiteOpenHelper {
+
+    final String CREAR_TABLA_PALABRA ="CREATE TABLE Palabra (idPalabra INTEGER, textoPalabra TEXT, activada INTEGER, patronVibracion INTEGER)";
+
+    public ConexionSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREAR_TABLA_PALABRA);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int versionAntigua, int versionNueva) {
+        db.execSQL("DROP TABLE IF EXISTS Palabra");
+    }
+}
