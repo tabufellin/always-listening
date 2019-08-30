4ac25470f888c826ee1a7b427804fd8bfbc667b6
pablofermarroquin <pablofermarroquin@gmail.com>
2019-08-30 14:55:33 -0600

    Conexion a SQLite

--- app/src/main/java/com/example/contactos/MainActivity.java
+++ app/src/main/java/com/example/contactos/MainActivity.java
@@ -12,10 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConexionSQLiteHelper conn = new ConexionSQLiteHelper(this, "BaseDeDatos", null, 1);
     }
 
     public void Anadir (View view){
         Intent anadir = new Intent(this, Anadir.class);
         startActivity(anadir);
     }
+
+    public void Anadir2 (View view){
+        Intent anadir = new Intent(this, Anadir.class);
+        startActivity(anadir);
+    }
+
+
+
 }
