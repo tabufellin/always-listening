9fe7575653567f9111dcbbe3a7786e162175b60a
Paula0020 <mpaula0020@hotmail.com>
2019-08-30 23:51:40 -0600

    Speech-To-Text

---
+++ app/src/main/java/com/example/contactos/entidades/SpeechT.java
@@ -0,0 +1,10 @@
+package com.example.contactos;
+
+import androidx.appcompat.app.AppCompatActivity;//??
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import java.util.Locale://?
+import android.widget.Toast;
+import android.os.Bundle;
+
