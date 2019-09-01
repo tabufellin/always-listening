5ca603b0304bd540bbcb65b9f717bf21d3024f37
Paula0020 <mpaula0020@hotmail.com>
2019-09-01 01:40:19 -0600

    new speech to text activity

---
+++ app/src/main/java/com/example/contactos/Speechtext.java
@@ -0,0 +1,40 @@
+package com.example.contactos;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+
+import java.util.Locale://?
+import android.widget.Toast;
+
+
+
+public class Speechtext extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
