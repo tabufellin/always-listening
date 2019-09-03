79fbaa650d473ccbaa98452a0eda11128a61d4b9
Paula0020 <mpaula0020@hotmail.com>
2019-09-02 23:35:02 -0600

    updates, clase SPEECHT cpn código nuevo

--- app/src/main/java/com/example/contactos/Speechtext.java
+++ app/src/main/java/com/example/contactos/Speechtext.java
@@ -1,5 +1,6 @@
 package com.example.contactos;
 
+import android.content.ActivityNotFoundException;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,13 +14,16 @@ import android.view.View;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
 
-import java.util.Locale://?
+import java.util.ArrayList;
+import java.util.Locale;//?
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 
-public class Speechtext extends AppCompatActivity {
 
+public class Speechtext extends AppCompatActivity {
+    TextView catp = (TextView)findViewById()(R.id.catch);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +31,46 @@ public class Speechtext extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        };
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        fab.setOnClickListener(new View.OnClickListener()) {
+            //@Override
+            public void onClick(View  view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }
+
+    public void microbutton(View vaw) {
+        if (vaw.getId() == R.id.microButton);
     }
 
-}
+        mostrarAudioImput();
+    }
+
+    public void mostrarAudioInput() {
+        Intent mod = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mod.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL), RecognizerIntent.LANGUAGE_MODEL_FREE_FORM;
+        mod.putExtra(RecognizerIntent.EXTRA_LANGUAGE), Locale.getDefault();
+
+        try {
+            startActivityForResult(mod, 100);
+        } catch (ActivityNotFoundException que) {
+
+            Toast.makeText(Speechtext.this, "oh no", Toast.LENGTH_LONG).show();
+        }}
+
+        public void onActivityResult ( int request_code, int result_code, Intent mod){
+            super.onActivityResult(request_code, result_code, mod);
+            switch (request_code) {
+                case 100:
+                    if (result_code == RESULT_OK && !=null)
+                {
+
+                    ArrayList<String> palabras = mod.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    catp.setText(catp.get(0));
+                }
+                break;
+            }
+        }
+
